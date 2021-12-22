fca814b11701accdce9f36b352db9151129142ea
Kimberly Munjal <kimi@Kimi.fritz.box>
2021-12-23 00:10:26 +0100

    Add visit counter

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-
+export class HomePage implements OnInit {
   inputContent: string;
+  visitCount: number = Number(localStorage.getItem("page_view"));
+
+
   constructor() {
     this.inputContent = 'Glass Card'
+  }
+
+  ngOnInit(): void {
+    if (this.visitCount) {
+      this.visitCount = Number(this.visitCount) + 1;
+      localStorage.setItem("page_view", String(this.visitCount));
+    } else {
+      this.visitCount = 1;
+      localStorage.setItem("page_view", String(1));
+    }
   }
 }
