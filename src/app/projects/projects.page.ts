c3e66a50ffbdea295dd1434466266f22af23a998
Kimberly Munjal <kimi@Kimi.fritz.box>
2021-12-23 23:17:21 +0100

    Add and style example projects

--- src/app/projects/projects.page.ts
+++ src/app/projects/projects.page.ts
@@ -1,11 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 
+enum Status {
+  'closed',
+  'wip',
+  'future'
+}
+
+class Project {
+  private readonly id: number;
+  public readonly status: string;
+  public title: string;
+  public description: string;
+  public beginning: Date;
+  public ending: Date;
+
+  constructor(status: string, title: string, description: string) {
+    this.status = status;
+    this.title = title;
+    this.description = description;
+  }
+}
+
 @Component({
   selector: 'app-projects',
   templateUrl: 'projects.page.html',
   styleUrls: ['projects.page.scss'],
 })
 export class ProjectsPage implements OnInit {
+  projects: Project[] = [
+    new Project(Status[0], 'Feedbacksystem', 'A ticket system for my university that was developped by and for students'),
+    new Project(Status[0], 'My Name Brand', 'CI/CD for my portfolio page')
+
+  ];
+
   constructor() {
 
   }
